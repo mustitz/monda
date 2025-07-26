2e01553c7788e6f2009e44c2c93cbed8f55b4d7a
Mustitz <mustitz@gmail.com>
2025-07-26 11:49:24 +0000

    Add Parsec.

--- hask/app/Main.hs
+++ hask/app/Main.hs
@@ -1,4 +1,17 @@
 module Main where
 
+import Text.Parsec
+import Text.Parsec.String
+
+number :: Parser Int
+number = read <$> many1 digit
+
+numbers :: Parser [Int]
+numbers = number `sepBy` char ','
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  let input = "1,2,3,42"
+  case parse numbers "" input of
+    Left err -> print err
+    Right nums -> print nums
